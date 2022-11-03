3d807115d9efe57b4bc3b1167fb1972d98a8b41b
Pedro Palugan <88800549+pedropalugan@users.noreply.github.com>
2022-11-03 18:45:27 -0300

    Update index.js

--- cliente/src/Paginas/login/Redefinir/index.js
+++ cliente/src/Paginas/login/Redefinir/index.js
@@ -10,38 +10,41 @@ function App() {
   const [senha, setSenha] = useState('')
   const [cargo, setCargo] = useState('')
   const [senhaConfirmada, setSenhaConfirmada] = useState('')
-  
+
   const validar = () => {
     if (email != null | email != undefined) {
       Axios.put('http://localhost:3000/recuperarSenha/mandarEmail', {
-        email : email
+        email: email
       }).then((response) => response.data)
-      .then((response) => {
-        if(response.error === true){
-          alert(response.msg)
-        }
-        else{
-          setCargo(response.tabela)
-          console.log(response)
-        }
-      })
-      return(setShow(true))
-      
+        .then((response) => {
+          if (response.error === true) {
+            alert(response.msg)
+          }
+          else {
+            setCargo(response.tabela)
+            console.log(response)
+          }
+        })
+      return (setShow(true))
+
     } else {
-      return ( window.alert('CPF inválido! Verifique suas credenciais!')) ;
+      return (window.alert('CPF inválido! Verifique suas credenciais!'));
     }
   };
 
   const redefinir = () => {
-    console.log(cargo)
-    console.log(email)
-    Axios.put('http://localhost:3000/recuperarSenha/verificarCodigo', {
-      codigo : token,
-      senha : senha,
-      email : email,
-      cargo : cargo
-    }).then((response) => response.data)
-    .then((response) => console.log(response))
+    if (senha === senhaConfirmada) {
+      Axios.put('http://localhost:3000/recuperarSenha/verificarCodigo', {
+        codigo: token,
+        senha: senha,
+        email: email,
+        cargo: cargo
+      }).then((response) => response.data)
+        .then((response) => console.log(response))
+    }
+    else{
+      alert("As senhas informadas não são as mesmas")
+    }
   }
 
   return (
@@ -53,9 +56,9 @@ function App() {
               <Form.Group className="mb-3 " controlId="formBasicEmail" >
                 <Form.Label>Email:</Form.Label>
                 <Form.Control required type="email" placeholder="Enter email" value={email}
-                onChange={(e) => setEmail(e.target.value)} />
+                  onChange={(e) => setEmail(e.target.value)} />
               </Form.Group>
-              
+
               <Button
                 variant="success"
                 type="button"
@@ -66,45 +69,45 @@ function App() {
               </Button>
 
 
-             
+
             </div>
           </Form>
         </Card.Body>
       </Card>
       <Modal show={show} onHide={() => setShow(false)}>
-          <Modal.Header closeButton>
-            <Modal.Title >Redefinir Senha</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Form onSubmit={{}}>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                
-                <Form.Label>Token:</Form.Label>
-                <Form.Control required placeholder="" value = {token} onChange={(e) => setToken(e.target.value)}/>
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Senha:</Form.Label>
-                <Form.Control type="password" required placeholder="" value={senha} onChange={(e) => setSenha(e.target.value)}/>
-              </Form.Group>
+        <Modal.Header closeButton>
+          <Modal.Title >Redefinir Senha</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form onSubmit={{}}>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
 
-              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                <Form.Label>Confirmar senha:</Form.Label>
-                <Form.Control type="password" required placeholder="" value={senhaConfirmada} 
+              <Form.Label>Token:</Form.Label>
+              <Form.Control required placeholder="" value={token} onChange={(e) => setToken(e.target.value)} />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Senha:</Form.Label>
+              <Form.Control type="password" required placeholder="" value={senha} onChange={(e) => setSenha(e.target.value)} />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Confirmar senha:</Form.Label>
+              <Form.Control type="password" required placeholder="" value={senhaConfirmada}
                 onChange={(e) => setSenhaConfirmada(e.target.value)} />
-              </Form.Group>
-              
-              
-              <Modal.Footer>
-                <Button variant="secondary" onClick={() => setShow(false)}>
-                  Fechar
-                </Button>
-                <Button variant="success" onClick={redefinir}>
-                  Enviar
-                </Button>
-              </Modal.Footer>
-            </Form>
-          </Modal.Body>
-        </Modal>
+            </Form.Group>
+
+
+            <Modal.Footer>
+              <Button variant="secondary" onClick={() => setShow(false)}>
+                Fechar
+              </Button>
+              <Button variant="success" onClick={redefinir}>
+                Enviar
+              </Button>
+            </Modal.Footer>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </Container>
   );
 }
