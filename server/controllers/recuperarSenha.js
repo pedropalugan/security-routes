a76ae2ea883e19e0f6556b5b1aac451c63096a34
Pedro Palugan <88800549+pedropalugan@users.noreply.github.com>
2022-11-03 18:44:59 -0300

    Update recuperarSenha.js

--- server/controllers/recuperarSenha.js
+++ server/controllers/recuperarSenha.js
@@ -14,21 +14,21 @@ const recoverModels = require('../models/recoverPswdModels')
 const tabelas = [atletaModels, medicoConvModels, medicoModels, gestorAdminModels, gestorModels]
 
 const obj = {
-    'ATLETA' : atletaModels,
-    'Médico' : atletaModels,
-    'Médico convidado' : atletaModels,
-    'gestor admin' : atletaModels,
-    'gestor' : atletaModels,
+    'ATLETA': atletaModels,
+    'Médico': atletaModels,
+    'Médico convidado': atletaModels,
+    'gestor admin': atletaModels,
+    'gestor': atletaModels,
 }
 
-class recuperarSenhaControllers{
+class recuperarSenhaControllers {
 
-    static async mandarEmail(req, res){
+    static async mandarEmail(req, res) {
         let email = req.body.email
         await database.sync()
-        for(let x = 0; x < tabelas.length; x++){
-            let request = await tabelas[x].findOne({ raw : true , where:{email : email} })
-            if(request){
+        for (let x = 0; x < tabelas.length; x++) {
+            let request = await tabelas[x].findOne({ raw: true, where: { email: email } })
+            if (request) {
                 let array = []
                 for (let x = 0; x < 6; x++) {
                     let numero = Math.floor(Math.random() * 10) //cria o código aletório
@@ -40,15 +40,15 @@ class recuperarSenhaControllers{
                     user: 'pedro.palugan@senaisp.edu.br',
                     pass: 'Peppalugan12'
                 }
-    
-    
+
+
                 let transporter = nodemailer.createTransport({
                     service: "hotmail",
                     auth: {
                         user: testAccount.user,
                         pass: testAccount.pass,
                     },
-                });   
+                });
                 let cargo = Object.values(request)[9]
                 let info = await transporter.sendMail({ //manda o email com o código gerado randomicamente
                     from: testAccount.user,
@@ -65,30 +65,38 @@ class recuperarSenhaControllers{
                         email: email
                     })
                     console.log(cargo)
-                    res.status(200).json({msg : 'Email enviado', error : false, tabela : cargo})
+                    res.status(200).json({ msg: 'Email enviado', error: false, tabela: cargo })
                 })
 
 
                 break
             }
-            else if(request == null && x == tabelas.length - 1){
-                res.status(200).json({msg : 'Nao existe esse email', error : true})
+            else if (request == null && x == tabelas.length - 1) {
+                res.status(200).json({ msg: 'Nao existe esse email', error: true })
             }
         }
     }
 
-    static async verificarCodigo(req, res){
+    static async verificarCodigo(req, res) {
         await database.sync()
-        let request = await recoverModels.findOne({ raw: true, where:{email : req.body.email} })
-        const salt = bcrypt.genSaltSync(2)
-        const hash = bcrypt.hashSync(req.body.senha.toString(), salt)
-        req.body.senha = hash
-        if(req.body.codigo === request.codigo){
-            await obj[req.body.cargo].update(req.body, {where : {email : req.body.email}})
-            .then(() => res.status(200).json({msg : 'Email atualizado com sucesso'}))
-            .catch((err) => res.send(err))
+        let request = await recoverModels.findOne({ raw: true, where: { email: req.body.email } })
+        if (request === {} | request === undefined | request === null) {
+            res.send("O email não foi encontrado")
         }
-        
+        else {
+            const salt = bcrypt.genSaltSync(2)
+            const hash = bcrypt.hashSync(req.body.senha.toString(), salt)
+            req.body.senha = hash
+            if (req.body.codigo === request.codigo) {
+                await obj[req.body.cargo].update(req.body, { where: { email: req.body.email } })
+                    .then(() => res.status(200).json({ msg: 'Email atualizado com sucesso' }))
+                    .catch((err) => res.send(err))
+            }
+            else {
+                res.send("Código não é o mesmo enviado")
+            }
+        }
+
     }
 
 }
