fc454fcaee241409b0c23b69605dca1268c8e08d
Pedro Palugan <88800549+pedropalugan@users.noreply.github.com>
2022-11-04 17:13:10 -0300

    Add files via upload

--- cliente/src/Rotas/index.js
+++ cliente/src/Rotas/index.js
@@ -1,22 +1,27 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Login from '../Paginas/login';
-import Redefinir from '../Paginas/login/Redefinir';
-import Home from '../Paginas/';
-import Header from '../componentes/Header/Header';
-import Footer from '../componentes/Footer/Footer';
-import PageNotFound from '../Paginas/404';
-
-
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "../Paginas/login";
+import Redefinir from "../Paginas/login/Redefinir";
+import Header from "../componentes/Header/Header";
+import Footer from "../componentes/Footer/Footer";
+import PageNotFound from "../Paginas/404";
+import Atleta from "../Paginas/atleta";
+import Medico from "../Paginas/medico";
+import Gestor from "../Paginas/gestor";
+import MedicoParceiro from "../Paginas/medico_parceiro";
 
 const Rotas = () => (
   <BrowserRouter>
     <Routes>
-      <Route exact path='/'    element={ <Login /> } />
-      <Route exact path='/home/:cargo' element={ <Home /> } />
-      <Route exact path='/redefinir' element={ <Home /> } />
-      <Route path='*' element={<PageNotFound />} />
+      <Route exact path="/" element={<Login />} />
+      <Route exact path="/redefinir" element={<Redefinir />} />
+      <Route path="*" element={<PageNotFound />} />
+      <Route exact path="/atleta" element={<Atleta />} />
+      <Route exact path="/medico" element={<Medico />} />
+      <Route exact path="/gestor" element={<Gestor />} />
+      <Route exact path="/médico parceiro" element={<MedicoParceiro />} />
     </Routes>
+    <Footer />
   </BrowserRouter>
 );
 
